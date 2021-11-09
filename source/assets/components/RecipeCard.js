eee98fc7f6a4e45ce534ac9bb46be1e1a627f830
EdwinLauMack <elaumack@ucsd.edu>
2021-11-09 03:14:29 -0800

    Good Progress on Favorites Page

---
+++ source/assets/components/RecipeCard.js
@@ -0,0 +1,328 @@
+class RecipeCard extends HTMLElement {
+    constructor() {
+      super();
+      // Part 1 Expose - TODO
+      this.attachShadow({mode: 'open'});
+      // You'll want to attach the shadow DOM here
+    }
+  
+    set data(data) {
+      // This is the CSS that you'll use for your recipe cards
+      const styleElem = document.createElement('style');
+      const styles = `
+
+      *, *::before, *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+      }
+
+      .card {
+        overflow: hidden;
+        box-shadow: 0px 2px 20px gray;  
+        border-radius: 0.2rem;
+        display:flex;
+        flex-direction: column;
+        justify-content: space-between;
+        cursor: pointer;
+        transition: transform 200ms ease-in;
+      }
+      .card__image {
+        height: 12rem;
+        width: 100%;
+        object-fit: cover;
+      }
+      .card__title {
+        padding: 1rem;
+      }
+      .card__description {
+        padding: 0 1rem;
+      }
+      .card__btn {
+        padding: 1rem;
+        font-family: inherit;
+        font-weight: bold;
+        font-size: 1rem;
+        margin: 1rem;
+        border: 2px solid red;
+        background: transparent;
+        color: red;
+        border-radius: 0.2rem;  
+      }
+      .card:hover {
+        transform: scale(1.02);
+      }
+      .card:hover .card__btn {
+        background: red;
+        color: white;
+      } 
+    `;
+      styleElem.innerHTML = styles;
+  
+      // Here's the root element that you'll want to attach all of your other elements to
+      const card = document.createElement("article");
+
+      //first div of card
+      const reciCard = document.createElement("div");
+      reciCard.classList.add("card");
+
+      //body of the card
+      const card_body = document.createElement("div");
+      card_body.classList.add("card__body");
+
+      /* ************************
+                Image 
+         ************************
+      */
+      const img = document.createElement("img");
+      img.classList.add("card__image");
+      const imgSrc = searchForKey(data, "thumbnail") || searchForKey(data, "thumbnailUrl") || searchForKey(data, "url"); 
+      const title = searchForKey(data, "headline");
+    
+      img.setAttribute("src", imgSrc);
+      img.setAttribute("alt", title);
+      card_body.append(img);
+
+      /* ************************
+                Title 
+         ************************
+      */
+      const titl = document.createElement("h2");
+      titl.classList.add("card__title");
+      titl.textContent = title;
+      card_body.appendChild(titl);
+      
+       /* ************************
+                Description 
+         ************************
+      */
+      const info = searchForKey(data, "description");
+      const description = document.createElement("p");
+      description.classList.add("card__description");
+      description.textContent = info;
+      card_body.appendChild(description);
+
+
+       /* ************************
+                Button 
+         ************************
+      */
+
+      const button = document.createElement("button");
+      button.classList.add("card__btn");
+      button.textContent = "View Recipe";
+
+      reciCard.appendChild(card_body);
+      reciCard.appendChild(button);
+
+
+      card.appendChild(reciCard);
+      
+
+
+
+  
+    //   const img = document.createElement('img');
+    //   // console.log(data);
+    //   const imgSrc = searchForKey(data, "thumbnail") || searchForKey(data, "thumbnailUrl") || searchForKey(data, "url"); //get the thumbnail url
+    //   const title = searchForKey(data, "headline");  //might change 
+    //   const organization = getOrganization(data) || searchForKey(data, "organization"); //get the organization name
+    //   const linkToArticle = getUrl(data) || searchForKey(data, 'mainEntityOfPage'); //get the url to the article)
+    //   const ratingV = searchForKey(data, "aggregateRating")?.ratingValue; //get the rating value of the recipe
+    //   const ratingC = searchForKey(data, "aggregateRating")?.ratingCount; //get the rating count of the recipe
+  
+    //   img.setAttribute("src", imgSrc);  //store the source of the image for the recipe
+    //   img.setAttribute("alt", title); //store the alternative text 
+    //   card.appendChild(img); //append to 'article'
+  
+    //   const titl = document.createElement('p'); //create the title element
+    //   titl.classList.add('title'); //add it to the class title 
+    //   const link = document.createElement('a'); //create the link element
+    //   link.setAttribute("href", linkToArticle); //set its link to the link of the article
+    //   link.textContent = title; //change the content of the link to its headline 
+    //   titl.appendChild(link); 
+    //   card.appendChild(titl);
+      
+    //   const org = document.createElement('p');
+    //   org.classList.add("organization");
+    //   org.textContent = organization; //change the organization name
+    //   card.appendChild(org); 
+  
+  
+    //   const rating = document.createElement('div');
+    //   rating.classList.add('rating');
+    //   const ratingScore = document.createElement('span');
+    //   rating.appendChild(ratingScore);
+  
+    //   if (ratingV) { //if the recipe has a review
+    //     const ratingCount = document.createElement('span');
+    //     const ratingImg = document.createElement('img');
+  
+    //     const ratingImgScore = Math.round(ratingV); //we round to the nearest integer in order to use one of the star svg's given
+  
+    //     ratingImg.setAttribute('src', `assets/images/icons/${ratingImgScore}-star.svg`); //set the new start svg
+  
+    //     ratingScore.textContent = Math.round(ratingV*100)/100; //we do this in order to evade having such a big decimal number (rounds to 2 decimal points)
+    //     ratingCount.textContent = `(${ratingC})`;  //we set the amount of reviews (count)
+  
+    //     rating.appendChild(ratingImg);
+    //     rating.appendChild(ratingCount);
+    //   } else {
+    //     ratingScore.textContent = "There are currently no reviews"; //if there is no ratingV then the recipe has no reviews
+    //   }
+    //   card.appendChild(rating)
+  
+    //   const time = document.createElement('time');
+    //   const totalTime = searchForKey(data, 'totalTime');
+    //   time.textContent = convertTime(totalTime);
+    //   card.appendChild(time);
+  
+    //   const ingredients = document.createElement('p');
+    //   ingredients.classList.add('ingredients');
+    //   const ingredientsList = searchForKey(data, 'recipeIngredient');
+    //   ingredients.textContent = createIngredientList(ingredientsList);
+    //   card.appendChild(ingredients);
+  
+      this.shadowRoot.appendChild(styleElem);
+      this.shadowRoot.appendChild(card);
+  
+      // Some functions that will be helpful here:
+      //    document.createElement()
+      //    document.querySelector()
+      //    element.classList.add()
+      //    element.setAttribute()
+      //    element.appendChild()
+      //    & All of the helper functions below
+  
+      // Make sure to attach your root element and styles to the shadow DOM you
+      // created in the constructor()
+  
+      // Part 1 Expose - TODO
+    }
+  }
+  
+  
+  /*********************************************************************/
+  /***                       Helper Functions:                       ***/
+  /***          Below are some functions I used when making          ***/
+  /***     the solution, feel free to use them or not, up to you     ***/
+  /*********************************************************************/
+  
+  /**
+   * Recursively search for a key nested somewhere inside an object
+   * @param {Object} object the object with which you'd like to search
+   * @param {String} key the key that you are looking for in the object
+   * @returns {*} the value of the found key
+   */
+  function searchForKey(object, key) {
+    var value;
+    Object.keys(object).some(function (k) {
+      if (k === key) {
+        value = object[k];
+        return true;
+      }
+      if (object[k] && typeof object[k] === 'object') {
+        value = searchForKey(object[k], key);
+        return value !== undefined;
+      }
+    });
+    return value;
+  }
+  
+  /**
+   * Extract the URL from the given recipe schema JSON object
+   * @param {Object} data Raw recipe JSON to find the URL of
+   * @returns {String} If found, it returns the URL as a string, otherwise null
+   */
+  function getUrl(data) {
+    if (data.url) return data.url;
+    if (data['@graph']) {
+      for (let i = 0; i < data['@graph'].length; i++) {
+        if (data['@graph'][i]['@type'] == 'Article') return data['@graph'][i]['@id'];
+      }
+    };
+    return null;
+  }
+  
+  /**
+   * Similar to getUrl(), this function extracts the organizations name from the
+   * schema JSON object. It's not in a standard location so this function helps.
+   * @param {Object} data Raw recipe JSON to find the org string of
+   * @returns {String} If found, it retuns the name of the org as a string, otherwise null
+   */
+  function getOrganization(data) {
+    if (data.publisher?.name) return data.publisher?.name;
+    if (data['@graph']) {
+      for (let i = 0; i < data['@graph'].length; i++) {
+        if (data['@graph'][i]['@type'] == 'Organization') {
+          return data['@graph'][i].name;
+        }
+      }
+    };
+    return null;
+  }
+  
+  /**
+   * Converts ISO 8061 time strings to regular english time strings.
+   * Not perfect but it works for this lab
+   * @param {String} time time string to format
+   * @return {String} formatted time string
+   */
+  function convertTime(time) {
+    let timeStr = '';
+  
+    // Remove the 'PT'
+    time = time.slice(2);
+  
+    let timeArr = time.split('');
+    if (time.includes('H')) {
+      for (let i = 0; i < timeArr.length; i++) {
+        if (timeArr[i] == 'H') return `${timeStr} hr`;
+        timeStr += timeArr[i];
+      }
+    } else {
+      for (let i = 0; i < timeArr.length; i++) {
+        if (timeArr[i] == 'M') return `${timeStr} min`;
+        timeStr += timeArr[i];
+      }
+    }
+  
+    return '';
+  }
+  
+  /**
+   * Takes in a list of ingredients raw from imported data and returns a neatly
+   * formatted comma separated list.
+   * @param {Array} ingredientArr The raw unprocessed array of ingredients from the
+   *                              imported data
+   * @return {String} the string comma separate list of ingredients from the array
+   */
+  function createIngredientList(ingredientArr) {
+    let finalIngredientList = '';
+  
+    /**
+     * Removes the quantity and measurement from an ingredient string.
+     * This isn't perfect, it makes the assumption that there will always be a quantity
+     * (sometimes there isn't, so this would fail on something like '2 apples' or 'Some olive oil').
+     * For the purposes of this lab you don't have to worry about those cases.
+     * @param {String} ingredient the raw ingredient string you'd like to process
+     * @return {String} the ingredient without the measurement & quantity 
+     * (e.g. '1 cup flour' returns 'flour')
+     */
+    function _removeQtyAndMeasurement(ingredient) {
+      return ingredient.split(' ').splice(2).join(' ');
+    }
+  
+    ingredientArr.forEach(ingredient => {
+      ingredient = _removeQtyAndMeasurement(ingredient);
+      finalIngredientList += `${ingredient}, `;
+    });
+  
+    // The .slice(0,-2) here gets ride of the extra ', ' added to the last ingredient
+    return finalIngredientList.slice(0, -2);
+  }
+  
+  // Define the Class so you can use it as a custom element.
+  // This is critical, leave this here and don't touch it
+  customElements.define('recipe-card', RecipeCard);
+
---
+++ source/assets/components/main.js
@@ -0,0 +1,151 @@
+// main.js
+
+// Here is where the recipes that you will fetch.
+// Feel free to add your own here for part 2, if they are local files simply add their path as a string.
+const recipes = [
+    'https://introweb.tech/assets/json/ghostCookies.json',
+    'https://introweb.tech/assets/json/birthdayCake.json',
+    'https://introweb.tech/assets/json/chocolateChip.json',
+    'assets/recipes/roasted_brussel_sprouts.json',
+    'assets/recipes/greek_chicken_skewers.json',
+    'assets/recipes/chicken_apple.json'
+  ];
+  
+
+  
+  //const combinedRecipes = recipes.concat(newRecipes);
+  //console.log(combinedRecipes);
+  
+  // Once all of the recipes that were specified above have been fetched, their
+  // data will be added to this object below. You may use whatever you like for the
+  // keys as long as it's unique, one suggestion might but the URL itself
+  const recipeData = {};
+  
+  window.addEventListener('DOMContentLoaded', init);
+  
+  // This is the first function to be called, so when you are tracing your code start here.
+  async function init() {
+    // fetch the recipes and wait for them to load
+    let fetchSuccessful = await fetchRecipes();
+    // if they didn't successfully load, quit the function
+    if (!fetchSuccessful) {
+      console.log('Recipe fetch unsuccessful');
+      return;
+    };
+  
+    
+  
+    // Add the first three recipe cards to the page
+    createRecipeCards();
+    // Make the "Show more" button functional
+    // bindShowMore();
+  }
+  
+  async function fetchRecipes() {
+    return new Promise((resolve, reject) => {
+  
+  
+      const allPromises = []; //store all the promises inside promise array
+      for (let i = 0; i < recipes.length; i++)
+      {
+        allPromises.push(
+          fetch(recipes[i])
+          .then(response => response.json())
+          .then(data => {
+            recipeData[recipes[i]] = data;
+          })
+          .catch(() => reject(false)) //if we catch an error we reject the promise 
+        );
+      }
+      Promise.all(allPromises).then(() => { //function that waits for all three promises to resolve (link: https://www.javascripttutorial.net/es6/javascript-promise-all/)
+        resolve(true);
+      });
+    });
+  }
+  
+  
+  
+      // This function is called for you up above
+      // From this function, you are going to fetch each of the recipes in the 'recipes' array above.
+      // Once you have that data, store it in the 'recipeData' object. You can use whatever you like
+      // for the keys. Once everything in the array has been successfully fetched, call the resolve(true)
+      // callback function to resolve this promise. If there's any error fetching any of the items, call
+      // the reject(false) function.  
+  
+      // For part 2 - note that you can fetch local files as well, so store any JSON files you'd like to fetch
+      // in the recipes folder and fetch them from there. You'll need to add their paths to the recipes array.
+  
+      // Part 1 Expose - TODO
+  
+  
+  function createRecipeCards() {
+  
+   
+    // This function is called for you up above.
+    // From within this function you can access the recipe data from the JSON 
+    // files with the recipeData Object above. Make sure you only display the 
+    // three recipes we give you, you'll use the bindShowMore() function to
+    // show any others you've added when the user clicks on the "Show more" button.
+  
+    // Part 1 Expose - TODO
+      const main = document.querySelector('main');
+      for (let i = 0; i < recipes.length; i++) //iterate through the whole recipes array
+      {
+      const CardtoCreate = document.createElement('recipe-card'); //create a card for it
+      CardtoCreate.data = recipeData[recipes[i]]; //store the data into the card
+      main.appendChild(CardtoCreate); //append to main so it shows up
+
+      
+      }
+  }
+  
+  /*function bindShowMore() {
+  
+    const main = document.querySelector('main');
+    const button = document.querySelector('button');
+    const arrow = document.querySelector('#button-wrapper>img');
+    
+    const addRecipes = [] //used to we can later delete them
+    let moreOrLess = false; //when set to false means it's not expanded, else it is expanded
+  
+    button.addEventListener('click', () => {
+        
+      if (moreOrLess)
+        moreOrLess = false;
+      else
+        moreOrLess = true;
+  
+    if (moreOrLess) //if we are on show more
+    {
+      for (let i = 0; i < newRecipes.length; i++) //traverse through the new recipes
+      {
+        const CardtoCreate = document.createElement('recipe-card');
+        CardtoCreate.data = recipeData[newRecipes[i]]; 
+        addRecipes.push(CardtoCreate); //create a recipe card and put it in our addRecipes array (this is used in order to remove them later from main)
+      }
+  
+      for (let j = 0; j < addRecipes.length; j++) //traverse through the addRecipes array and append every card into main
+      {
+        main.appendChild(addRecipes[j]);
+      }
+      button.textContent = "Show Less"; //change the text to "show less"
+      arrow.style.transform = "rotate(180deg)"; //rotate the arrow upside down
+    }
+    else
+    {
+      for (let k = 0; k < addRecipes.length; k++) //else if we are on show less we traverse through the addrecipes array
+      {
+        main.removeChild(addRecipes[k]); //we get that card and we remove it from main 
+      }
+      while (addRecipes.length != 0) //while addRecipes array is not empty we pop it (empty it out) 
+      {
+          addRecipes.pop();
+      }
+      button.textContent = "Show More"; //change the text of the button 
+      arrow.style.transform = "rotate(0deg)"; //rotate the arrow to its original position
+  
+    } 
+    })
+  
+  }
+  */    
