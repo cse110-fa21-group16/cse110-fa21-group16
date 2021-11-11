b7905c6c4998f2df47092e61d552d2bfce939894
Tai Le <tnl001@ucsd.edu>
2021-11-10 18:18:25 -0800

    cleaned up and backed up files

--- source/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", init);
-
-function init() {
-    // testing edit recipe component
-    let editSection = document.querySelector("section[class='edit-section']");
-    let editPage = document.createElement("edit-recipe");
-    editPage.data = "testing";
-    editSection.appendChild(editPage);
-}
