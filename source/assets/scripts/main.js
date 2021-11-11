8cc068d9db50c29659b2a3e502e4cd266232ea8c
amc01 <amc001@ucsd.edu>
2021-11-10 21:05:20 -0800

    removed extra files

--- source/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", init);
-
-function init() {
-    // testing edit recipe component
-    let nutritionSection = document.querySelector("section[class='nutrition-page']");
-    let nutritionPage = document.createElement("nutrition-page");
-    nutritionPage.data = "testing";
-    nutritionSection.appendChild(nutritionPage);
-}
