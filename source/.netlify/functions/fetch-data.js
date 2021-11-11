9fddead5c65ea42b4952650c7b95c21b68100e17
Tai Le <tnl001@ucsd.edu>
2021-11-10 23:02:20 -0800

    updated netlify api call

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -1,13 +1,25 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 const APIKey = process.env.API_KEY;
 
 exports.handler = function(event, context, callback) {
-    const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        resolve(true);
-        console.log(data);
-        return data;
-    }).catch(error => reject(false));
+    // const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
+    // fetch(url)
+    // .then(response => response.json())
+    // .then(data => {
+    //     resolve(true);
+    //     console.log(data);
+    //     return data;
+    // }).catch(error => reject(false));
+    let data = {
+		name:'ray',
+		foo:[1,2,4,6],
+		time:Date.now()
+	};
+	
+	console.log('data is '+JSON.stringify(data));
+
+	callback(null, {
+		statusCode:200,
+		body:JSON.stringify(data)
+	});
 }
