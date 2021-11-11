1689f0187a4a8e2c973150e4611b28ef5a1121a2
Tai Le <tnl001@ucsd.edu>
2021-11-10 23:46:30 -0800

    Updated css for edit/add page

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -1,7 +1,7 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 const APIKey = process.env.API_KEY;
 
-exports.handler = function(event, context, callback) {
+export function handler(event, context, callback) {
     const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
     fetch(url)
     .then(response => response.json())
