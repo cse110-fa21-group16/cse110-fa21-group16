fe748e70b3e25df7078b2777bbca4c5563effb94
Tai Le <tnl001@ucsd.edu>
2021-11-11 00:11:48 -0800

    Updated css for edit/add page

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -2,12 +2,14 @@ const fetch = require("axios");
 const APIKey = process.env.API_KEY;
 
 exports.handler = function(event, context, callback) {
-    const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        resolve(true);
-        console.log(data);
-        return data;
-    }).catch(error => reject(false));
+    return new Promise((resolve, reject) => {
+        const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            resolve(true);
+            return data;
+        }).catch(error => reject(false));
+    });
 }
