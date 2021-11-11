fed48e18335b8c0a9a8892fde3c1b456a9ceddbe
Tai Le <tnl001@ucsd.edu>
2021-11-11 00:42:01 -0800

    Updated css for edit/add page

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -1,17 +1,29 @@
 const fetch = require("node-fetch-commonjs");
 const APIKey = process.env.API_KEY;
 
-exports.handler = function(event, context, callback) {
-    const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
-    return new Promise((resolve, reject) => {
-        fetch(url)
-        .then((response) => {
-            callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(response.data)
-            });
-            console.log(response.data);
-        })
-        .catch(error => reject(false));
-    });
-}
+// exports.handler = function(event, context, callback) {
+//     const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
+//     return new Promise((resolve, reject) => {
+//         fetch(url)
+//         .then((response) => {
+//             callback(null, {
+//                 statusCode: 200,
+//                 body: JSON.stringify(response.data)
+//             });
+//             console.log(response.data);
+//         })
+//         .catch(error => reject(false));
+//     });
+// }
+
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data.joke,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
+};
