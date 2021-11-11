c5a3a86119b1ee8a6470cd6dedb1b7ef9580f9fa
Tai Le <tnl001@ucsd.edu>
2021-11-11 00:37:15 -0800

    Updated css for edit/add page

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -4,6 +4,13 @@ const APIKey = process.env.API_KEY;
 exports.handler = function(event, context, callback) {
     const url = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey}&number=30`;
     return new Promise((resolve, reject) => {
-        fetch(url).catch(error => reject(false));
+        fetch(url)
+        .then((response) => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(response.data)
+            })
+        })
+        .catch(error => reject(false));
     });
 }
