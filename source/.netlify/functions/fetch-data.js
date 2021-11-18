cdd1d92785c156a1868ed884fa81b4db703bd906
Tai Le <tnl001@ucsd.edu>
2021-11-17 22:45:18 -0800

    Added cases for loading api

--- source/.netlify/functions/fetch-data.js
+++ source/.netlify/functions/fetch-data.js
@@ -9,14 +9,21 @@ const API_Endpoint = `https://api.spoonacular.com/recipes/random?apiKey=${APIKey
  * @returns 
  */
 exports.handler = async (event, context) => {
-    return new Promise((resolve, reject) => {
-		fetch(API_Endpoint)
-        .then(res => res.json())
-        .then(data => {
-            resolve({
-                statusCode: 200,
-                body: JSON.stringify(data)
-            })
-        }).catch(error => reject(false));
-	});
+    if (window.localStorage.getItem("feaRecipeArray") === null) { // fetch from api if no key found in local storage
+        return new Promise((resolve, reject) => {
+            fetch(API_Endpoint)
+            .then(res => res.json())
+            .then(data => {
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify(data)
+                })
+            }).catch(error => reject(false));
+        });
+    } else {
+        return { // else load from storage
+            statusCode: 200,
+            body: JSON.stringify(window.localStorage.getItem("feaRecipeArray"))
+        }
+    }
 };
