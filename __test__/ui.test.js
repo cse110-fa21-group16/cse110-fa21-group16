66419d7bf99cb6b4a09f89f096364d98c86dfd9e
Tai Le <tnl001@ucsd.edu>
2021-11-26 15:31:53 -0800

    Added a dummy jest test

--- __test__/ui.test.js
+++ __test__/ui.test.js
@@ -2,4 +2,7 @@ describe("UI testing", () => {
     // beforeAll(async () => {
     //     await page.goto("URL of site here");
     // });
+    it("Dummy test", async () => {
+        expect(10).toBe(10);
+    });
 });
