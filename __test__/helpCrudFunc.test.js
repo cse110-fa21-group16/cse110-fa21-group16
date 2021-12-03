b15a881e4ab56d245ba87779cf39ba2979ed825b
Tai Le <tnl001@ucsd.edu>
2021-12-03 01:00:41 -0800

    Added comments

--- __test__/helpCrudFunc.test.js
+++ __test__/helpCrudFunc.test.js
@@ -1,3 +1,6 @@
+import { checkFav, rmFav, addFav, addMy, updateMy, rmMy } from "../source/assets/scripts/helpCrudFunc.js";
+
+// Example of how to import and use localstorage in jest unit test
 const localStorage = require("localStorage");
 localStorage.setItem("testing", "matched");
 test("test localStorage", () => {
