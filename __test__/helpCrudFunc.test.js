73158b4c570cd37ca4ccc21dd5aed129e48d76e1
Andrew Park <ajpark@ucsd.edu>
2021-12-03 18:44:20 -0800

    added unit tests for rmFav function

--- __test__/helpCrudFunc.test.js
+++ __test__/helpCrudFunc.test.js
@@ -17,7 +17,7 @@ test("test1 addMy Function", () => {
     addMy(data, nextMyRecipeID, myRecipeArray);
     expect(localStorage.getItem('nextMyRecipeID')).toBe('4');
     expect(localStorage.getItem('myRecipeArray')).toBe('[{\"id\":3333},{\"id\":2345},{\"id\":3}]')
-})
+});
 //test2 of addMy. test edge case of addMy Function, checking null case
 let data3 = {"id": 7};
 let nextMyRecipeID2 = null;
@@ -26,4 +26,76 @@ test("test2 addMy Function", () => {
     addMy(data3, nextMyRecipeID2, myRecipeArray2);
     expect(localStorage.getItem('nextMyRecipeID')).toBe('1');
     expect(localStorage.getItem('myRecipeArray')).toBe('[{\"id\":0}]')
-})
+});
+
+let recipeOne = {"title" : "Chocolate Chip Cookies"};
+let recipeTwo = {"title" : "French Toast"};
+let recipeThree = {"title" : "Buffalo Wings"};
+let recipeFour = {"title" : "Pumpkin Pie"};
+let recipeFive = {"title" : "Chicken Alfredo"};
+
+let favRecipeArray1 = [recipeOne, recipeTwo, recipeThree, recipeFour, recipeFive];
+//unit test 1 of rmFav function
+test("test removing an existing recipe", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray1));
+    rmFav("Chocolate Chip Cookies", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    expect(updatedArr).toEqual(expect.not.stringMatching("Chocolate Chip Cookies"));
+    expect(updatedArr.length).toBe(4);
+});
+
+
+let favRecipeArray2 = [recipeOne, recipeTwo, recipeThree, recipeFour, recipeFive];
+//unit test 2 of rmFav function
+test("test removing a favorite recipe that does not exist", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray2));
+    rmFav("Chicken Tenders", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    //recipe should remain the same
+    expect(updatedArr).toEqual(favRecipeArray2);
+    expect(updatedArr.length).toBe(5);
+});
+
+let favRecipeArray3 = [recipeOne];
+//unit test 3 of rmFav function
+test("test removing the only favorite recipe", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray3));
+    rmFav("Chocolate Chip Cookies", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    expect(updatedArr.length).toBe(0);
+});
+
+let favRecipeArray4 = [recipeOne, recipeTwo];
+//unit test 4 of rmFav function
+test("test removing a favorite recipe from an array of two favorites", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray4));
+    rmFav("French Toast", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    expect(updatedArr).toEqual(expect.not.stringMatching("French Toast"));
+    expect(updatedArr.length).toBe(1);
+});
+
+let favRecipeArray5 = [recipeOne, recipeTwo, recipeThree, recipeFour, recipeFive];
+let expectedArray = [{"title" : "French Toast"}, {"title" : "Buffalo Wings"}]
+//unit test 5 of rmFav function
+test("test removing three favorite recipes repeatedly from an array of five favorites", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray5));
+    rmFav("Chocolate Chip Cookies", JSON.parse(localStorage.getItem("favRecipeArray")));
+    rmFav("Chicken Alfredo", JSON.parse(localStorage.getItem("favRecipeArray")));
+    rmFav("Pumpkin Pie", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    expect(updatedArr).toStrictEqual(expectedArray);
+    expect(updatedArr.length).toBe(2);
+});
+
+let favRecipeArray6 = [recipeOne, recipeTwo, recipeThree, recipeFour];
+//unit test 6 of rmFav function
+test("test removing all four favorite recipes", () => {
+    localStorage.setItem("favRecipeArray", JSON.stringify(favRecipeArray6));
+    rmFav("Chocolate Chip Cookies", JSON.parse(localStorage.getItem("favRecipeArray")));
+    rmFav("Buffalo Wings", JSON.parse(localStorage.getItem("favRecipeArray")));
+    rmFav("Pumpkin Pie", JSON.parse(localStorage.getItem("favRecipeArray")));
+    rmFav("French Toast", JSON.parse(localStorage.getItem("favRecipeArray")));
+    let updatedArr = JSON.parse(localStorage.getItem("favRecipeArray"));
+    expect(updatedArr.length).toBe(0);
+});
